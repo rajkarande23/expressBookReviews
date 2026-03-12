414f11c0751ef2ffa684d25a36941b43cb160412
rajkarande23 <raj.karande23@vit.edu>
2026-03-12 14:11:35 -0400

    Added Axios async routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
 
@@ -18,7 +20,7 @@ public_users.post("/register", (req,res) => {
   if(isValid(username)){
     users.push({username:username,password:password});
     return res.status(200).json({message:"User successfully registered"});
-  }
+  } 
   else{
     return res.status(404).json({message:"User already exists"});
   }
@@ -34,22 +36,23 @@ public_users.get('/',function (req, res) {
 });
 
 
-// Get book by ISBN
+// Get book details by ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
 
   const isbn = req.params.isbn;
-
   return res.status(200).json(books[isbn]);
 
 });
-  
+
 
 // Get books by author
 public_users.get('/author/:author',function (req, res) {
 
   const author = req.params.author;
 
-  let filtered_books = Object.values(books).filter(book => book.author === author);
+  let filtered_books = Object.values(books).filter(
+    book => book.author === author
+  );
 
   return res.status(200).json(filtered_books);
 
@@ -61,7 +64,9 @@ public_users.get('/title/:title',function (req, res) {
 
   const title = req.params.title;
 
-  let filtered_books = Object.values(books).filter(book => book.title === title);
+  let filtered_books = Object.values(books).filter(
+    book => book.title === title
+  );
 
   return res.status(200).json(filtered_books);
 
@@ -72,8 +77,74 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
 
   const isbn = req.params.isbn;
-
   return res.status(200).json(books[isbn].reviews);
+
+});
+
+
+// ------------------------------
+// Axios + Async APIs (Tasks 10–13)
+// ------------------------------
+
+
+// Get all books using Axios
+public_users.get('/asyncbooks', async function(req,res){
+
+  try{
+    const response = await axios.get("http://localhost:5000/");
+    res.send(response.data);
+  }
+  catch(error){
+    res.status(500).json({message:"Error retrieving books"});
+  }
+
+});
+
+
+// Get book by ISBN using Axios
+public_users.get('/asyncisbn/:isbn', async function(req,res){
+
+  try{
+    const response = await axios.get(
+      `http://localhost:5000/isbn/${req.params.isbn}`
+    );
+    res.send(response.data);
+  }
+  catch(error){
+    res.status(500).json({message:"Error retrieving book"});
+  }
+
+});
+
+
+// Get books by author using Axios
+public_users.get('/asyncauthor/:author', async function(req,res){
+
+  try{
+    const response = await axios.get(
+      `http://localhost:5000/author/${req.params.author}`
+    );
+    res.send(response.data);
+  }
+  catch(error){
+    res.status(500).json({message:"Error retrieving books by author"});
+  }
+
+});
+
+
+// Get books by title using Axios
+public_users.get('/asynctitle/:title', async function(req,res){
+
+  try{
+    const response = await axios.get(
+      `http://localhost:5000/title/${req.params.title}`
+    );
+    res.send(response.data);
+  }
+  catch(error){
+    res.status(500).json({message:"Error retrieving books by title"});
+  }
 
 });
 
